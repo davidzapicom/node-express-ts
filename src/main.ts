5763a4b1223350b504c75e9e09772bd263b4a7de
David Zapico <david.zapico@the-cocktail.com>
2024-06-19 20:43:19 +0200

    Init

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/ping', (_, res) => {
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
